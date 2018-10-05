112c5287655304e2418a4865c7471ef9efda39b2
Training_b6b.01.03 <Training_b6b.01.03@accenture.com>
2018-10-05 12:13:55 +0530

    terstingThirdBuild

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -21,7 +21,7 @@ public class HelloTest {
 	public void beforeTest() {
 		//	System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
 		driver = new ChromeDriver();
-		System.setProperty("webdriver.chrome.driver","C:\code\HelloWorld\src\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\code\\HelloWorld\\src\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://localhost:8080/WAR/EAR/");
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
